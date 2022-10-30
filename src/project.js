cad678f18efb59a2fe3c5a2df41951a7f6b9d5bc
Jacob Wanner <106052324+jacobwanner@users.noreply.github.com>
2022-10-30 13:44:18 -0400

    inital files upload

---
+++ src/project.js
@@ -0,0 +1,13 @@
+const project = () => {
+    const create = () => {
+        let title = '';
+    }
+    const edit = () => {
+
+    }
+    const erase = () => {
+        let all = () => {
+            // deletes the whole project
+        }
+    }
+}
