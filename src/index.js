96ff2cda9232fbd4962ea8304151eb1bbf5c2fb2
Jacob Wanner <106052324+jacobwanner@users.noreply.github.com>
2022-11-01 19:42:20 -0400

    working on getting projects array working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello there');
+let project_0 = {
+    title: prompt("what is the title of this project? "),
+    description: prompt("Please Give a brief description of the project: "),
+    dueDate: prompt("what is the due date :"),
+}
+const existingProjects = [];
+existingProjects.push(project_0)
+
+console.log(existingProjects(0).title)
