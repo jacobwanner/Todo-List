70b374ba2ab7fb5c62d20123c3b905d706177fe9
Jacob Wanner <106052324+jacobwanner@users.noreply.github.com>
2022-11-04 22:26:08 -0400

    create button works
    when create button is pressed, new object is added to array

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-let project_0 = {
-    title: prompt("what is the title of this project? "),
-    description: prompt("Please Give a brief description of the project: "),
-    dueDate: prompt("what is the due date :"),
-}
-const existingProjects = [];
-existingProjects.push(project_0)
+import './domStuff';
+import { create } from './project';
 
-console.log(existingProjects(0).title)
+createButton.addEventListener("click", () => {
+    create();
+}); 
